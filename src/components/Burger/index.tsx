4342e9b3e9a80a0517c480b8c900699663b002ee
JessZanewich <jessvoneuw@gmail.com>
2022-03-20 14:05:18 -0600

    move to src folder for cleaner root directory

---
+++ src/components/Burger/index.tsx
@@ -0,0 +1,13 @@
+import { StyledBurger } from './Burger.styles';
+
+const Burger = () => {
+  return (
+    <StyledBurger>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  );
+};
+
+export default Burger;
